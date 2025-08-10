d11455d4bea000cf6e311b76333339a6e0d9216f
user <user@raz.lap>
2025-08-09 20:25:52 -0700

    try to talk to meter

--- querymeter.py
+++ querymeter.py
@@ -1,4 +1,11 @@
 #!/bin/env python3
+import serial
+import time
+
+config=open('querymeter.conf','r').read().splitlines()
+SERIAL=config[0] # first line of querymeter.conf should be like /dev/ttyUSB0
+logfile=open('querymeter_'+time.strftime('%Y%m%d%H%M%S')+'.csv','w')
+serialPort = serial.Serial(port=SERIAL,baudrate=1200, bytesize=8, parity='N', stopbits=1, timeout=5000, xonxoff=0, rtscts=0, dsrdtr=0, write_timeout=0.1)
 
 def send_message(message):
     packet = [len(message) + 3] # firts byte is length including itself and checksum
@@ -6,7 +13,20 @@ def send_message(message):
     crc = crc16_arc(packet) # crc16 of length byte plus message
     packet.append(int(crc % 256)) # LSB
     packet.append(int(crc / 256)) # MSB
+    serialPort.write(packet)
     return packet
+
+def receive_message():
+    a = serialPort.read_all()
+    if a[0] != len(a):
+        print('invalid packet recieved: ',end='')
+        print(a)
+        return False
+    if crc16_arc(a[:-2]) !=    a[-1] * 256 + a[-2]:
+        print(a)
+        print('CRC was ' + str(a[-1] * 256 + a[-2]) + ' but expected ' + str(crc16_arc(a[:-2])))
+        return False
+    return a
 
 def crc16_arc(data, offset=0, length=None):
     if length is None:
@@ -23,7 +43,35 @@ def crc16_arc(data, offset=0, length=None):
                 crc = crc >> 1
     return crc   
 
+def wait_for_response():
+    r = False
+    startWaitTime = time.time()
+    while (r == False) and (time.time() - startWaitTime < 5):
+        r = receive_message()
+    if r:
+        print(r)
+    else:
+        print('timed out waiting for a response')
+
+def send_break():
+    serialPort.break_condition = True # https://forums.raspberrypi.com/viewtopic.php?t=239406
+    time.sleep(0.110)
+    serialPort.break_condition = False
+    time.sleep(0.060)
+
 if __name__ == '__main__':
-    print('CRC:',hex(crc16_arc(bytes.fromhex('0B0000000000000009'))))
-    print('CRC:',hex(crc16_arc([0x0B,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x09])))
-    print(send_message([0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x09]))
+    #print('CRC:',hex(crc16_arc(bytes.fromhex('0B0000000000000009'))))
+    #print('CRC:',hex(crc16_arc([0x0B,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x09])))
+    send_break()
+    print('sent ' + str(send_message([0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x09])))
+    time.sleep(0.220)
+    send_break()
+    print('sent ' + str(send_message([0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x07,0x00])))
+    # now 0.553 seconds will go by before response
+    wait_for_response()
+    time.sleep(5.13)
+    send_break()
+    print('sent ' + str(send_message(['0x60','0x93','0x05','0x50','0x10','0x02','0xB6','0x03','0x00','0x00','0x00','0x00','0x00','0x00','0x00','0x01','0x00','0x00','0x00','0x00','0x00','0x00','0x00','0x00','0x22','0x01','0x00','0x00','0x74'])))
+    wait_for_response()
+    print('sent ' + str(send_message(['0x60','0x93','0x05','0x50','0x10','0x02','0xB6','0x05','0x00','0x00','0x00','0x00','0x00','0x00','0x00','0x00','0xA7','0x23','0xB1','0xC1','0xE9','0x4D','0xA6','0x8C'])))
+    wait_for_response()
